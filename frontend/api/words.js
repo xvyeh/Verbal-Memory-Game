897dab4f91253b5a561075af70b3da9ae29e4cae
yeh <147702782+xvyeh@users.noreply.github.com>
2026-04-14 10:18:10 +0200

    Update words.js

--- frontend/api/words.js
+++ frontend/api/words.js
@@ -2,7 +2,8 @@ export default function handler(req, res) {
   const words = [
     "apple", "banana", "car", "dog", "elephant",
     "forest", "guitar", "house", "island", "jungle",
-    "keyboard", "lamp", "mountain", "notebook", "ocean"
+    "keyboard", "lamp", "mountain", "notebook", "ocean", "goat",
+    "cat", "dog", "giraffe", "phone", "computer", "pokemon"
   ];
 
   const randomWord = words[Math.floor(Math.random() * words.length)];
